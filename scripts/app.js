08b82bb6e2249be49aefbe72708b858f3cd8e88f
Oleg Ivanov <78684921+ivanovsdesign@users.noreply.github.com>
2021-02-22 22:55:58 +0300

    Select DOM elements inside a class

--- scripts/app.js
+++ scripts/app.js
@@ -15,24 +15,64 @@ class UI {
         this.displayArea = document.querySelector(".display-area");
     }
 
+    //submitting forms
+
+    incomeSubmit(svalue){
+        const value = this.incomeInput.value;
+
+        if (value < 0 || value === "" || value === NaN){
+            this.incomeInput.classList.add("input-alert");
+            this.incomeInput.placeholder = "Enter a valid amount";
+            const self = this;
+            setTimeout(function(){
+                self.incomeInput.classList.remove("input-alert");
+                self.incomeInput.placeholder = "Enter your income";
+            }, 2000)
+        }
+        else {
+            this.incomeUpdate(svalue);
+        }
+    }
+
+    //updating visual elements
+
+    incomeUpdate(value){
+        this.visualIncome.textContent = `\$${value}`
+    }
+
+    expenseUpdate(value){
+        this.visualExpenses.textContent = `\$${value}`
+    }
+
+    budgetUpdate(value){
+        this.visualBudget.textContent = `\$${value}`
+    }
+
+    //manipulating expenses list
+
     addEntry(){
         const entries = ['kek'];
         let entry = document.createElement("div");
         entry.classList.add("display-entry");
         entry.innerHTML = `<p class="entry-title">${entries[0]}</p>
+        <p class="entry-amount">$120</p>
         <p class="entry-button" id="edit-btn">Edit</p>
         <p class="entry-button" id="delete-btn">Delete</p>`
         this.displayArea.appendChild(entry);
     }
 }
 
-const ui = new UI();
-const self = this;
+window.addEventListener("DOMContentLoaded", function() {
+    const ui = new UI();
+    var count = 0;
 
-ui.incomeBtn.addEventListener("click", function() {
-    ui.visualIncome.textContent = `\$${ui.incomeInput.value}`;
+    ui.incomeBtn.addEventListener("click", function() {
+        count = ui.incomeInput.value;
+        ui.incomeSubmit(count);
+    })
+
+    ui.expensesBtn.addEventListener("click", function() {
+        ui.addEntry();
+    })
 })
 
-ui.expensesBtn.addEventListener("click", function() {
-    ui.addEntry();
-})
